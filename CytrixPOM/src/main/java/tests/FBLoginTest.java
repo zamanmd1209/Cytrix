c953fd199ca16dd1ff0d9e51836c7e92d8bca3b4
zamanmd1209 <39254440+zamanmd1209@users.noreply.github.com>
2019-09-15 17:26:09 -0500

    first commit

---
+++ CytrixPOM/src/main/java/tests/FBLoginTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import pages.FbHomePage;
+import pages.FbLoginPage;
+
+public class FBLoginTest extends TestBase{
+	 
+	 @Test
+	 public void init() throws Exception{
+	 
+	 //driver.get("https://www.facebook.com");
+	 FbLoginPage loginpage = PageFactory.initElements(driver, FbLoginPage.class);
+	 loginpage.setEmail("rajkumarsmonline@gmail.com");
+	 loginpage.setPassword("raj123456");
+	 loginpage.clickOnLoginButton();
+	 
+	 FbHomePage homepage = PageFactory.initElements(driver, FbHomePage.class);
+	 homepage.clickOnProfileDropdown();
+	 homepage.verifyLoggedInUserNameText();
+	 homepage.clickOnLogoutLink(); 
+	 }
+	 
+	}
+	//Step 4: Creating testng.xml file
+	
+
+
---
+++ CytrixPOM/src/main/java/tests/TestBase.java
@@ -0,0 +1,38 @@
+package tests;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+	
+	public static WebDriver driver = null;
+	 
+	 @BeforeSuite
+	 public void initialize() throws IOException{
+	 
+	 System.setProperty("webdriver.chrome.driver", "./driverss/chromedriver.exe");
+	 driver = new ChromeDriver(); 
+	 //To maximize browser
+	                driver.manage().window().maximize();
+	         //Implicit wait
+	         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	 //To open facebook
+	                driver.get("https://www.facebook.com");
+	 
+	 }
+	 
+	 @AfterSuite
+	 //Test cleanup
+	 public void TeardownTest()
+	    {
+	        TestBase.driver.quit();
+	    }
+	 
+	
+
+}
